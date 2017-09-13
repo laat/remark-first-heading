59d257c12ec2c497b5319ff8c071f2e7d559f35c
Sigurd Fosseng <sigurd@fosseng.net>
2017-09-13 17:47:02 +0200

    prettier

--- src/remark-first-heading.js
+++ src/remark-first-heading.js
@@ -1,22 +1,20 @@
-var toString = require('mdast-util-to-string')
+var toString = require('mdast-util-to-string');
 
-module.exports = function attacher (options = {}) {
-  return function transformer (root) {
+module.exports = function attacher(options = {}) {
+  return function transformer(root) {
     let heading = {
       type: 'heading',
       depth: 1,
-      children: [
-        { type: 'text', value: options.heading }
-      ]
-    }
+      children: [{ type: 'text', value: options.heading }],
+    };
 
-    let first = root.children[0]
+    let first = root.children[0];
     if (first && first.type === 'heading') {
       if (toString(first) !== options.heading) {
-        root.children[0] = heading
+        root.children[0] = heading;
       }
     } else {
-      root.children.unshift(heading)
+      root.children.unshift(heading);
     }
-  }
-}
+  };
+};
--- src/remark-first-heading.spec.js
+++ src/remark-first-heading.spec.js
@@ -1,45 +1,64 @@
 /* global describe, it */
-import { expect } from 'chai'
-import remark from 'remark'
-import firstHeading from './remark-first-heading'
+import { expect } from 'chai';
+import remark from 'remark';
+import firstHeading from './remark-first-heading';
 
-function clean (multiline) {
-  return multiline.split('\n').splice(1).join('\n')
+function clean(multiline) {
+  return multiline
+    .split('\n')
+    .splice(1)
+    .join('\n');
 }
-function test (expected, actual) {
-  expect(process(expected)).to.eq(clean(actual))
+function test(expected, actual) {
+  expect(process(expected)).to.eq(clean(actual));
 }
-function process (string) {
-  const r = remark().use(firstHeading, {heading: 'heading'})
-  return r.processSync(string).toString()
+function process(string) {
+  const r = remark().use(firstHeading, { heading: 'heading' });
+  return r.processSync(string).toString();
 }
 
 describe('Remark First Heading', () => {
-  it('insert a heading', () => test('', `
+  it('insert a heading', () =>
+    test(
+      '',
+      `
 # heading
-`))
+`
+    ));
 
-  it('heading shuold stay the same', () => test(`
+  it('heading shuold stay the same', () =>
+    test(
+      `
 # heading
-`, `
+`,
+      `
 # heading
-`))
+`
+    ));
 
-  it('insert if first is not a heading', () => test(`
+  it('insert if first is not a heading', () =>
+    test(
+      `
 a text
-`, `
+`,
+      `
 # heading
 
 a text
-`))
+`
+    ));
 
-  it('replace wrong heading', () => test(`
+  it('replace wrong heading', () =>
+    test(
+      `
 # replace me
 
 a text
-`, `
+`,
+      `
 # heading
 
 a text
-`))
-})
+`
+    ));
+});
